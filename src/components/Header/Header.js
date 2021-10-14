db890b99bf3eb4f10deab7335306c0b08c63d0b0
SyedShajib <syedshajib55@gmail.com>
2021-10-14 08:28:49 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import logo from '../../images/logo.png';
 import './Header.css';
+
+
 const Header = () => {
     const { user, logOut } = useAuth();
     return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
-import './Login.css'
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import './Login.css';
+
 const Login = () => {
-    const {user, signInUsingGoogle} = useFirebase()
+    const { signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_uri = location.state?.from || '/shop';
+
+
+    const handleGoogleLogin = () => {
+        signInUsingGoogle()
+            .then(result => {
+                history.push(redirect_uri);
+            })
+    }
+
     return (
         <div className="login-form">
-             <div>
+            <div>
                 <h2>Login</h2>
                 <form>
                     <input type="email" name="" id="" placeholder="Your Email" />
                     <br />
                     <input type="password" name="" id="" />
                     <br />
-                    <input type="submit" value="Submit" />  
+                    <input type="submit" value="Submit" />
                 </form>
                 <p>new to ema-john website? <Link to="/register">Create Account</Link></p>
                 <div>-------or----------</div>
                 <button
                     className="btn-regular"
-                    onClick={signInUsingGoogle}
+                    onClick={handleGoogleLogin}
                 >Google Sign In</button>
             </div>
         </div>
